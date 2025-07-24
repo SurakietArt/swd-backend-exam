d7628b127939d0154fb332fd5461035ce24277fc
Surakiet Piyasakulkiet <surakiet.p@gmail.com>
2025-07-24 21:49:20 +0700

    feat: solveed number to roman.

--- 3_number_to_thai/main.py
+++ 3_number_to_thai/main.py
@@ -20,4 +20,49 @@ output = number can not less than 0
 class Solution:
 
     def number_to_thai(self, number: int) -> str:
-        pass
+        if number < 0:
+            return "number can not less than 0"
+        if number == 0:
+            return "ศูนย์"
+        if number > 10_000_000:
+            return "number must not more than 10,000,000"
+
+        digit_text = ["", "หนึ่ง", "สอง", "สาม", "สี่", "ห้า", "หก", "เจ็ด", "แปด", "เก้า"]
+        position_text = ["", "สิบ", "ร้อย", "พัน", "หมื่น", "แสน", "ล้าน"]
+
+        def convert(num: int) -> str:
+            result = ""
+            digits = [int(d) for d in str(num)]
+            length = len(digits)
+
+            for i in range(length):
+                d = digits[i]
+                pos = length - i - 1
+
+                if d == 0:
+                    continue
+
+                if pos == 0 and d == 1 and length > 1:
+                    result += "เอ็ด"
+                elif pos == 1 and d == 2:
+                    result += "ยี่" + position_text[pos]
+                elif pos == 1 and d == 1:
+                    result += position_text[pos]
+                else:
+                    result += digit_text[d] + position_text[pos]
+            return result
+
+        million = 1000000
+        if number < million:
+            return convert(number)
+        else:
+            millions = number // million
+            remainder = number % million
+            text = convert(millions) + "ล้าน"
+            if remainder > 0:
+                text += convert(remainder)
+            return text
+
+
+x = Solution().number_to_thai(1003751)
+print(x)
--- 4_number_to_roman/main.py
+++ 4_number_to_roman/main.py
@@ -18,6 +18,28 @@ output = number can not less than 0
 
 
 class Solution:
+    def number_to_roman(self, num: int) -> str:
+        if num <= 0:
+            return "number can not be less than or equal to 0"
+        number_to_roman = {
+            1000: "M",
+            900: "CM",
+            500: "D",
+            400: "CD",
+            100: "C",
+            90: "XC",
+            50: "L",
+            40: "XL",
+            10: "X",
+            9: "IX",
+            5: "V",
+            4: "IV",
+            1: "I"
+        }
 
-    def number_to_roman(self, number: int) -> str:
-        pass
+        result = ""
+        for value in number_to_roman.keys():
+            while num >= value:
+                result += number_to_roman[value]
+                num -= value
+        return result
